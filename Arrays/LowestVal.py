672a7810f5b42365f95bfe5df696c99318438bbf
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-10-04 23:33:19 +0530

    commit 5

---
+++ Arrays/LowestVal.py
@@ -0,0 +1,12 @@
+#it is a program to find the lowest value in an array
+
+myArr = [9,3,45,23,6,64,756,2]
+minValue =  myArr[0]
+
+for i in myArr:
+    i < minValue
+    minValue = i
+
+print(minValue)
+
+
