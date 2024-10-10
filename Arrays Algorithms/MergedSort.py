467ebbd531c9e722bc39fea0a44cca2c963a6477
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-10-11 00:47:13 +0530

    Fixed bugs in MergeSort

--- Arrays Algorithms/MergedSort.py
+++ Arrays Algorithms/MergedSort.py
@@ -12,11 +12,12 @@ def mergeSort(arr):
         return  merge(sortedLeft,sortedRight)
     
 
+
 def merge(left,right):
     result = []
     i = j = 0 
 
-    while( i <len(left) and j < len(right)):
+    while i < len(left) and j < len(right):
         if(left[i]<right[j]):
             result.append(left[i])
             i += 1
@@ -24,15 +25,12 @@ def merge(left,right):
             result.append(right[j])
             j += 1
         
-    
     result.extend(left[i:])
     result.extend(right[j:])
+
     return result
 
 
-unsortedArray = [8,2,5,3,4,7,6,1]
-print("Unsorted array : " ,  unsortedArray)
-
-SortedArray = (mergeSort(unsortedArray))
-
-print("Sorted array : " , SortedArray) 
+unsortedArr = [3, 7, 6, -10, 15, 23.5, 55, -13]
+sortedArr = mergeSort(unsortedArr)
+print("Sorted array:", sortedArr)
