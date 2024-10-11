97731d8439898bca97dc967b346faf21c009b0ae
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-10-11 19:16:45 +0530

    Array Methods

--- Arrays Algorithms/QuickSort.py
+++ Arrays Algorithms/QuickSort.py
@@ -1,6 +1,11 @@
 # Simply divinding the code into different methods to reduce our work 
 # first method is sorting and the second is for dividing and arranging the  array in the correct order
 
+
+
+# code is incorrect  because it is not considering the case when the array is already sorted or reverse sorted
+
+
 myarr = [8,2,5,3,9,4,7,6,1]
 
 def quickSort(myarr, low=0,high=None):
@@ -17,10 +22,10 @@ def quickSort(myarr, low=0,high=None):
 def partition(myarr , low, high):
     pivot = myarr[high] # pivot is the last element in the array
     i = low -1 
-     for j in range(low, high):
+    for j in range(low, high):
         if myarr[j] <= pivot:
             i += 1
-            myarr[i], array[j] = myarr[j], myarr[i]
+            myarr[i], myarr[j] = myarr[j], myarr[i]
 
     myarr[i+1], myarr[high] = myarr[high], myarr[i+1]
     return i+1
