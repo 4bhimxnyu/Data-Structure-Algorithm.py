197c3602c497ef95be1db78e0ce4246d11e1925e
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-10-18 19:28:22 +0530

    added isEmpty method

---
+++ Stacks and Queues/StackModern.py
@@ -0,0 +1,37 @@
+# here we are going to create the stack in the form of the modern list way . 
+# all the functions we will be commented out to get a clear understanding of the code  and to implement all the functions.
+
+
+stack =[]
+
+stack.append('James')
+stack.append('likes')
+stack.append('to')
+stack.append('Code')
+
+# here we are going to pop the elements from the stack .
+
+# stack.pop()
+
+#now we will change the index of the pop
+ 
+#stack.pop(0)
+
+#now we will push some new elements at the top of the stack
+
+#stack.append('and')
+#stack.append('Music')
+
+# now we will use the peek function for the code 
+
+#topElement = stack[len(stack)-1]
+#print(topElement)
+
+# now we will check if the stack is empty or not
+# if the stack is empty then it will return true otherwise it will return false.
+
+isEmpty = not bool(stack)
+print(isEmpty)
+
+
+
